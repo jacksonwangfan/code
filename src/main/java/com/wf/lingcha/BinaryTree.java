b9126abd6c265b631453b56b3be09f763c4ab796
wangfan <wangfanwangfan@didiglobal.com>
2025-12-09 13:19:33 +0800

    合并二叉树

--- src/main/java/com/wf/lingcha/BinaryTree.java
+++ src/main/java/com/wf/lingcha/BinaryTree.java
@@ -279,8 +279,24 @@ public class BinaryTree {
      * 合并二叉树 https://leetcode.cn/problems/merge-two-binary-trees/
      */
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
-        return null;
+        if (root1 == null) {
+            return root2;
+        }
+
+        if (root2 == null) {
+            return root1;
+        }
+
+        root1.val += root2.val;
+
+        root1.left = mergeTrees(root1.left, root2.left);
+        root1.right = mergeTrees(root1.right, root2.right);
+        return root1;
     }
+
+
+
+
 
 
 
