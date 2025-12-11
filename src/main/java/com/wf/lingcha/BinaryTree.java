bc7f7df0df50fcc3dc947dc09be7a3c4c9672eae
wangfan <wangfanwangfan@didiglobal.com>
2025-12-11 18:57:43 +0800

    而插数的路径总和

--- src/main/java/com/wf/lingcha/BinaryTree.java
+++ src/main/java/com/wf/lingcha/BinaryTree.java
@@ -123,6 +123,37 @@ public class BinaryTree {
         return  pathSum(root.left, targetSum, sum) || pathSum(root.right, targetSum, sum);
     }
 
+    /**
+     * 129. 求根节点到叶节点数字之和
+     * https://leetcode.cn/problems/sum-root-to-leaf-numbers/description/
+     * 给你一个二叉树的根节点 root ，树中每个节点都存放有一个 0 到 9 之间的数字。
+     * 每条从根节点到叶节点的路径都代表一个数字：
+     * 例如，从根节点到叶节点的路径 1 -> 2 -> 3 表示数字 123 。
+     * 计算从根节点到叶节点生成的 所有数字之和 。
+     * 叶节点 是指没有子节点的节点。
+     */
+    public int sumNumbers(TreeNode root) {
+        return sumNumbersSub(root, 0);
+    }
+
+    //遍历到到叶子节点之后的和
+    public int sumNumbersSub(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+
+        sum = sum * 10 + root.val;
+        //当是叶子节点的时候收集答案
+        if (root.left == null && root.right == null) {
+            return sum;
+        }
+
+       return sumNumbersSub(root.left, sum) + sumNumbersSub(root.right, sum);
+
+    }
+
+
+
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(3);
